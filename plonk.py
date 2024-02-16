fede2de416e05e4697b2c1def5fa208135021f14
giulio <giuliop@gmail.com>
2024-02-16 12:06:06 +0100

    Prover and Verifier completed

--- plonk.py
+++ plonk.py
@@ -57,7 +57,11 @@ def run_setup(circuit_filepath):
                   'k2': k2, 'H': H, 'H1': H1, 'H2': H2, }
 
     # TODO: if w_order > num_gates since w_order is the smallest power of 2
-    #       greater than num_gates, we need to pad the circuit
+    #       greater than num_gates, we need to pad the circuit so that n,
+    #       the number of gates, is equal to w_order
+    # Once implemented, we can remove the following assert
+    assert w_order == n
+
     a, b, c = build_permutations(H, H1, H2, circuit)
 
     QL_coeff = utils.interpolate(H, circuit['ql'], q)
@@ -145,18 +149,17 @@ def generate_proof(setup, public_inputs, trace):
 
     # Zh is the polynomial that is zero on all the elements of H
     # Zh = w^n - 1 ; where n is the order of w
-    coeff_Zh = [1] + [0] * (len(H) - 1) + [-1]
-    Zh = Poly(coeff_Zh)
+    Zh = Poly(x**n - 1)
 
     left, right, output = [trace[x] for x in ['left', 'right', 'output']]
 
-    A = utils.interpolate(H, left, q)
-    B = utils.interpolate(H, right, q)
-    C = utils.interpolate(H, output, q)
+    A = Poly(utils.interpolate(H, left, q))
+    B = Poly(utils.interpolate(H, right, q))
+    C = Poly(utils.interpolate(H, output, q))
 
-    A = Poly(b1 * x + b2) * Zh + Poly(A)
-    B = Poly(b3 * x + b4) * Zh + Poly(B)
-    C = Poly(b5 * x + b6) * Zh + Poly(C)
+    A = Poly(b1 * x + b2) * Zh + A
+    B = Poly(b3 * x + b4) * Zh + B
+    C = Poly(b5 * x + b6) * Zh + C
 
     com_A = kzg.commit(A, gp)
     com_B = kzg.commit(B, gp)
@@ -206,7 +209,7 @@ def generate_proof(setup, public_inputs, trace):
     QL, QR, QM, QO, QC = [Poly(setup['circuit'][x]) for x in
         ['QL_coeff', 'QR_coeff', 'QM_coeff', 'QO_coeff', 'QC_coeff']]
 
-    # Zw is Z(wX)
+    # Zw is Z(Xw)
     Z_coeff = Z.all_coeffs()
     Zw = Poly([(pow(H[1], len(Z_coeff) - 1 - i, q) * coeff) % q
                 for (i, coeff) in enumerate(Z_coeff)])
@@ -218,13 +221,17 @@ def generate_proof(setup, public_inputs, trace):
 
     T1 = A * B * QM + A * QL + B * QR + C * QO + PI + QC
 
-    T2 = Poly(alfa * ((A + beta * x + gamma) * (B + beta * k1 * x + gamma)
-                    * (C + beta * k2 * x + gamma) * Z))
+    T2 = Poly(alfa * ((A + beta * x + gamma)
+                      * (B + beta * k1 * x + gamma)
+                      * (C + beta * k2 * x + gamma)
+                      * Z))
 
-    T3 = Poly(-1 * alfa * ((A + beta * S1 + gamma) * (B + beta * S2 + gamma)
-                   * (C + beta * S3 + gamma) * Zw))
+    T3 = Poly(-alfa * ((A + beta * S1 + gamma)
+                       * (B + beta * S2 + gamma)
+                       * (C + beta * S3 + gamma)
+                       * Zw))
 
-    T4 = Poly(alfa**2 * ((Z - 1) * L1))
+    T4 = Poly(alfa**2 * (Z - 1) * L1)
 
     T_num = T1 + T2 + T3 + T4
     T, rem = T_num.div(Zh)
@@ -232,14 +239,13 @@ def generate_proof(setup, public_inputs, trace):
 
     T_coeff = [x % q for x in T.all_coeffs()]
 
-    Tlo_coeff = T_coeff[-n:]
-    Tmid_coeff = T_coeff[-2*n:-n]
     Thi_coeff = T_coeff[:-2*n]
+    Tmid_coeff = T_coeff[-2*n:-n]
+    Tlo_coeff = T_coeff[-n:]
 
     Tlo = Poly(Tlo_coeff) + Poly(b10 * x**n)
-    Tmid = Poly(Tmid_coeff) + Poly(-b10 + b11* x**n)
-    Thi = Poly(Thi_coeff) + Poly([-b11])
-
+    Tmid = Poly(Tmid_coeff) + Poly(-b10 + b11 * x**n)
+    Thi = Poly(Poly(Thi_coeff) - b11)
 
     com_Tlo, com_Tmid, com_Thi = [
             kzg.commit(x, gp) for x in [Tlo, Tmid, Thi]]
@@ -256,35 +262,46 @@ def generate_proof(setup, public_inputs, trace):
     s1_= S1.subs(x, zeta) % q
     s2_= S2.subs(x, zeta) % q
     zw_= Zw.subs(x, zeta) % q
-    pi_ = PI.subs(x, zeta) % q
-    l1_ = L1.subs(x, zeta) % q
-    zh_ = Zh.subs(x, zeta) % q
 
-    transcript += str(a_) + str(b_) + str(c_) + str(s1_) + str(s2_) + str(zw_)
+    transcript += (str(a_) + str(b_) + str(c_) + str(s1_)
+                   + str(s2_) + str(zw_))
 
     # ROUND 5
 
     v = utils.fiat_shamir(transcript, q)
 
+    pi_ = PI.subs(x, zeta) % q
+    l1_ = L1.subs(x, zeta) % q
+    zh_ = Zh.subs(x, zeta) % q
+
     R = Poly(
             a_ * b_ * QM + a_ * QL + b_ * QR + c_ * QO + pi_ + QC
-            + alfa * ((a_ + beta * zeta + gamma) * (b_ + beta * k1 * zeta + gamma)
+            + alfa * ((a_ + beta * zeta + gamma)
+                      * (b_ + beta * k1 * zeta + gamma)
                       * (c_ + beta * k2 * zeta + gamma) * Z)
-            - alfa * ((a_ + beta * s1_ + gamma) * (b_ + beta * s2_ + gamma)
+            - alfa * ((a_ + beta * s1_ + gamma)
+                      * (b_ + beta * s2_ + gamma)
                       * (c_ + beta * S3 + gamma) * zw_)
             + alfa**2 * ((Z - 1) * l1_)
             - zh_ * (Tlo + zeta**n * Tmid + zeta**(2*n) * Thi)
     )
 
-    Wzeta, rem = Poly(R + v * (A - a_) + v**2 * (B - b_) + v**3 * (C - c_)
-                     + v**4 * (S1 - s1_) + v**5 * (S2 - s2_)).div(Poly(x - zeta))
+    Wzeta, rem = Poly(R
+                      + v * (A - a_)
+                      + v**2 * (B - b_)
+                      + v**3 * (C - c_)
+                      + v**4 * (S1 - s1_)
+                      + v**5 * (S2 - s2_)
+                      ).div(Poly(x - zeta))
     assert rem == 0
 
     Wzeta_w, rem = Poly(Z - zw_).div(Poly(x - zeta * w))
     assert rem == 0
 
     com_Wzeta, com_Wzeta_w = [kzg.commit(x, gp) for x in [Wzeta, Wzeta_w]]
+
     transcript += str(com_Wzeta) + str(com_Wzeta_w)
+    u = utils.fiat_shamir(transcript, q)
 
     proof = {'com_A': com_A, 'com_B': com_B, 'com_C': com_C, 'com_Z': com_Z,
              'com_Tlo': com_Tlo, 'com_Tmid': com_Tmid, 'com_Thi': com_Thi,
@@ -292,6 +309,186 @@ def generate_proof(setup, public_inputs, trace):
              'b_': b_, 'c_': c_, 's1_': s1_, 's2_': s2_, 'zw_': zw_}
 
     return proof
+
+def verifier_preprocess(setup):
+    """Preprocess the common input for the verifier.
+
+    Args:
+        setup: A dict with the common input for the prover and verifier
+               coming from the run_setup function.
+
+    Returns:
+        A dict with the preprocessed input for the verifier:
+        - the commitments: com_QM, com_QL, com_QR, com_QO, com_QC,
+          com_S1, com_S2, com_S3
+        - gp: the trusted setup paramenters of the KZG commitment scheme
+        - order: the curve order
+        - n: the number of gates
+        - generators: the generators for the polynomial interpolation
+        - transcript: str(setup), the initial transcript to generate
+          the Fiat-Shamir challenge
+    """
+    a, b, c, = [setup['circuit'][x] for x in ['a', 'b', 'c']]
+    gp = setup['gp']
+    q = setup['order']
+    H = setup['generators']['H']
+    S1 = Poly(utils.interpolate(H, a, q))
+    S2 = Poly(utils.interpolate(H, b, q))
+    S3 = Poly(utils.interpolate(H, c, q))
+
+    return {
+        'com_QM': kzg.commit(setup['circuit']['QM_coeff'], gp),
+        'com_QL': kzg.commit(setup['circuit']['QL_coeff'], gp),
+        'com_QR': kzg.commit(setup['circuit']['QR_coeff'], gp),
+        'com_QO': kzg.commit(setup['circuit']['QO_coeff'], gp),
+        'com_QC': kzg.commit(setup['circuit']['QC_coeff'], gp),
+        'com_S1': kzg.commit(S1, gp),
+        'com_S2': kzg.commit(S2, gp),
+        'com_S3': kzg.commit(S3, gp),
+        'gp': gp, 'order': q, 'n': setup['num_gates'],
+        'generators': setup['generators'],
+        'transcript': str(setup)
+    }
+
+def verify_proof(preprocessed, public_inputs, proof):
+    """Verify the given proof.
+
+    Args:
+        preprocessed: A dict with the preprocessed input for the verifier
+        public_inputs: A list with the public inputs for the proof
+        proof: A proof object that can be verified by the verifier
+
+    Returns:
+        True if the proof is valid, False otherwise
+    """
+    transcript = preprocessed['transcript']
+    q = preprocessed['order']
+    n = preprocessed['n']
+    G1 = preprocessed['gp'][0][0]
+    G2 = preprocessed['gp'][1][0]
+    G2_x = preprocessed['gp'][1][1]
+    w, H, k1, k2 = [preprocessed['generators'][x]
+                    for x in ['w', 'H', 'k1', 'k2']]
+    com_QM, com_QL, com_QR, com_QO, com_QC, com_S1, com_S2, com_S3 = [
+        preprocessed[x] for x in ['com_QM', 'com_QL', 'com_QR', 'com_QO',
+                                  'com_QC', 'com_S1', 'com_S2', 'com_S3']]
+
+    try:
+        commitments = [proof[com] for com in [
+            'com_A', 'com_B', 'com_C', 'com_Z', 'com_Tlo',
+            'com_Tmid', 'com_Thi', 'com_Wzeta', 'com_Wzeta_w']]
+
+        values = [proof[x] for x in [
+            'a_', 'b_', 'c_', 's1_', 's2_', 'zw_']]
+
+    except KeyError:
+        return False
+
+    if not (all([kzg.curve.is_on_curve(p, kzg.curve.b)
+                 for p in commitments])
+            and all([0 <= v < q for v in values])
+            and all([0 <= v < q for v in public_inputs])):
+        return False
+
+    (com_A, com_B, com_C, com_Z, com_Tlo, com_Tmid, com_Thi, com_Wzeta,
+     com_Wzeta_w) = commitments
+    a_, b_, c_, s1_, s2_, zw_ = values
+
+    transcript += str(public_inputs)
+    transcript += str(com_A) + str(com_B) + str(com_C)
+
+    beta = utils.fiat_shamir([transcript, 0], q)
+    gamma = utils.fiat_shamir([transcript, 1], q)
+
+    transcript += str(com_Z)
+
+    alfa = utils.fiat_shamir(transcript, q)
+
+    transcript += (str(com_Tlo) + str(com_Tmid) + str(com_Thi))
+
+    zeta = utils.fiat_shamir(transcript, q)
+
+    transcript += (str(a_) + str(b_) + str(c_) + str(s1_)
+                   + str(s2_) + str(zw_))
+
+    v = utils.fiat_shamir(transcript, q)
+
+    transcript += str(com_Wzeta) + str(com_Wzeta_w)
+
+    u = utils.fiat_shamir(transcript, q)
+
+    Zh_ = (zeta**n - 1) % q
+
+    l1_ = sympy.Mod(1 * Zh_ * sympy.mod_inverse(n * (zeta - 1), q), q)
+
+    x = sympy.symbols('x')
+    PI = Poly(utils.interpolate(H, public_inputs, q))
+    pi_ = PI.subs(x, zeta) % q
+
+    r0_ = sympy.Mod((pi_
+                     - l1_ * alfa**2
+                     - alfa
+                     * (a_ + beta * s1_ + gamma)
+                     * (b_ + beta * s2_ + gamma)
+                     * (c_  + gamma)
+                     * zw_), q)
+
+    cmul = kzg.curve.multiply
+    cneg = kzg.curve.neg
+    def csum(*args):
+        # add all the arguments
+        acc = args[0]
+        for arg in args[1:]:
+            acc = kzg.curve.add(acc, arg)
+        return acc
+
+    com_D = csum(cmul(com_QM, a_ * b_ % q),
+                 cmul(com_QL, a_),
+                 cmul(com_QR, b_),
+                 cmul(com_QO, c_),
+                 com_QC,
+                 cmul(com_Z,
+                      sympy.Mod((a_ + beta * zeta + gamma)
+                                * (b_ + beta * k1 * zeta + gamma)
+                                * (c_ + beta * k2 * zeta + gamma)
+                                * alfa
+                                + l1_ * alfa**2 + u,
+                                q)),
+                 cneg(cmul(com_S3,
+                           sympy.Mod((a_ + beta * s1_ + gamma)
+                                     * (b_ + beta * s2_ + gamma)
+                                     * alfa * beta * zw_,
+                                     q))),
+                 cneg(cmul(csum(com_Tlo,
+                                cmul(com_Tmid, zeta**n % q),
+                                cmul(com_Thi, zeta**(2*n) % q)),
+                           Zh_)))
+
+    com_F = csum(com_D,
+                 cmul(com_A, v),
+                 cmul(com_B, v**2),
+                 cmul(com_C, v**3),
+                 cmul(com_S1, v**4),
+                 cmul(com_S2, v**5))
+
+    com_E = cmul(G1,
+                 sympy.Mod(-r0_
+                           + v * a_
+                           + v**2 * b_
+                           + v**3 * c_
+                           + v**4 * s1_
+                           + v**5 * s2_
+                           + u * zw_, q))
+
+    e1 = kzg.pairing(G2_x, csum(com_Wzeta,
+                                cmul(com_Wzeta_w, u)))
+
+    e2 = kzg.pairing(G2, csum(cmul(com_Wzeta, zeta),
+                              cmul(com_Wzeta_w, (u * zeta * w) % q),
+                              com_F,
+                              cneg(com_E)))
+
+    return e1 == e2
 
 
 def test():
@@ -305,8 +502,11 @@ def test():
     public_inputs = []
 
     proof = generate_proof(setup, public_inputs, trace)
-    print(proof)
+
+    preprocessed = verifier_preprocess(setup)
+    assert verify_proof(preprocessed, public_inputs, proof)
 
 
 if __name__ == '__main__':
     test()
+    print('All tests passed')
